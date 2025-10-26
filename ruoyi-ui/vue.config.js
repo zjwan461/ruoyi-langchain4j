cbba0b0249d195340884f5dfdc5995581bad94b8
jerry <826935261@qq.com>
2025-10-26 18:39:46 +0800

    重构项目结构

--- ruoyi-ui/vue.config.js
+++ ruoyi-ui/vue.config.js
@@ -12,7 +12,7 @@ const name = process.env.VUE_APP_TITLE || '若依AI智能体系统' // 网页标
 
 const baseUrl = 'http://localhost:8080' // 后端接口
 
-const port = process.env.port || process.env.npm_config_port || 80 // 端口
+const port = process.env.port || process.env.npm_config_port || 81 // 端口
 
 // vue.config.js 配置说明
 //官方vue.config.js 参考文档 https://cli.vuejs.org/zh/config/#css-loaderoptions
